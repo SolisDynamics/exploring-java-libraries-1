eac76aa15e06a0efb74203023caa8a866c2a4845
Solis Dynamics <meg-bjk@hotmail.com>
2026-04-13 18:16:53 +0300

    Update ReflectionExample.java

--- examples/reflection/ReflectionExample.java
+++ examples/reflection/ReflectionExample.java
@@ -1,1 +1,7 @@
-
+public class ReflectionExample {
+    public static void main(String[] args) {
+        Class<?> clazz = String.class;
+        System.out.println("Class name: " + clazz.getName());
+        System.out.println("Methods count: " + clazz.getMethods().length);
+    }
+}
