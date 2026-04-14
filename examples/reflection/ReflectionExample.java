068e31478f6b21ef62a4f6c0d89f48dd1dc0e5fb
Solis Dynamics <meg-bjk@hotmail.com>
2026-04-14 10:56:03 +0300

    Update and rename ReflectionExample.java to b15_ClassLoaderExampe.jva

--- examples/reflection/ReflectionExample.java
+++
@@ -1,7 +0,0 @@
-public class ReflectionExample {
-    public static void main(String[] args) {
-        Class<?> clazz = String.class;
-        System.out.println("Class name: " + clazz.getName());
-        System.out.println("Methods count: " + clazz.getMethods().length);
-    }
-}
